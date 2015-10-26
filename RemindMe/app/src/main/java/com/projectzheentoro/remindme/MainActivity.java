c6944bf76a3f4edd756cb9d153b25a05dd8f48f4
Zheentoro Kulmamat <Zheentoro@gmail.com>
2015-10-26 12:43:54 +0600

    Created MainActivity class and layout

---
+++ RemindMe/app/src/main/java/com/projectzheentoro/remindme/MainActivity.java
@@ -0,0 +1,17 @@
+package com.projectzheentoro.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.zheentoro.remindme.R;
+
+/**
+ * Created by Zheentoro on 26.10.2015.
+ */
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
